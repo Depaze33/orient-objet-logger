bc9ae77b9dc17e4ff1c9e483dfb5aa52a533ad2f
Clarisse Otmani <clarisseotm@gmail.com>
2024-07-09 14:53:27 +0200

    first commit + finish exercice

---
+++ src/main/java/fr/afpa/employees/Employee.java
@@ -0,0 +1,271 @@
+package fr.afpa.employees;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/*
+Objectif :
+L’objectif de ce TP est de concevoir un programme en console basé sur une approche objet et
+permettant de gérer (vraiment très basiquement) des salariés d’une entreprise.
+
+Vous allez écrire une classe représentant les salariés d'une entreprise.
+Vous pourrez vous aider du PDF disponible sur pour obtenir des informations sur la façon de faire.
+*/
+
+class Employee {
+	/**
+	 * Matricule de l'employé
+	 */
+	// Definir les attribut
+	private String registrationNumber;
+	private String lastName;
+	private String firstName;
+	private double salary;
+	private String birthDay;
+	private final int socialRate = 30;
+	private static final Logger logger = LoggerFactory.getLogger(Employee.class);
+
+
+	// compléter les attributs comme présenté dans le PDF
+	// Appeler les objet
+	// compléter le constructeur de la classe
+	public Employee(String registrationNumber, String lastName, String firstName, double salary, String birthDay) {
+		this.registrationNumber = registrationNumber;
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.salary = salary;
+		this.birthDay = birthDay;// LocalDate.parse(birthDay);
+
+	}
+
+	// implémenter les setters et getters de la classe (permet d'accéder aux
+	// attributs privés)
+	// decalare les getters
+	public String getRegistrationNumber() {
+		return registrationNumber;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public String getBirthDay() {
+		return birthDay;
+	}
+
+	// declare les setters
+	public void setRegistrationNumber(String registrationNumber)  {
+		// definir les exeptions
+		if(!checkRegistrationNumber(registrationNumber)){
+			IllegalArgumentException e = new IllegalArgumentException("Le matricule n'est pas correctement formate");
+			logger.error(e.getMessage(), e);
+			throw e;
+
+		}
+		this.registrationNumber = registrationNumber;
+
+	}
+
+	public void setLastName(String lastName) throws Exception {
+		boolean isLastName = checklastName(lastName);
+		if (isLastName == true) {
+			this.lastName = lastName;
+
+		} else {
+			IllegalArgumentException e = new IllegalArgumentException("Le nom saisie n'est pas correct");
+			logger.error(e.getMessage(),e);
+			throw e;
+			// throw new Exception("Le nom n'est pas correctement rentré");
+		}
+
+	}
+
+	public void setFirstName(String firstName) throws Exception {
+		boolean isFirstName = checkFirstName(firstName);
+		if (isFirstName == true) {
+
+			this.firstName = firstName;
+		} else {
+			IllegalArgumentException e = new IllegalArgumentException("Le prenom saisie n'est pas correct");
+			logger.error(e.getMessage(),e);
+			throw e;
+		}
+
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public void setBirthDay(String birthDay) throws Exception {
+		boolean isDate = checkBirthDay(birthDay);
+		if (isDate == true) {
+			this.birthDay = birthDay;
+		} else {
+			IllegalArgumentException e = new IllegalArgumentException("La date de naissance n'est pas correctement formate");
+			logger.error(e.getMessage(),e);
+			throw e;
+		}
+
+	}
+
+	// implémenter la méthode "toString()" qui renvoie une chaîne de caractère
+	// qui représente un objet de la classe employé
+	// plus d'information sur la méthode "toString()" ->
+	// https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
+
+	@Override
+	public String toString() {
+
+		return "Employee { "
+				+ " \nMatricule " + getRegistrationNumber()
+				+ " \nNom " + getLastName()
+				+ " \nPrenom " + getFirstName()
+				+ " \nSalaire " + getSalary()
+				+ " \nDate d'anniversaire " + getBirthDay()
+				+ "\nSalaire net " + netSalary()
+				+ "\nJours avant prochain anniversaire " + daysBeforeBirthday()
+				+ '}';
+
+	}
+
+	// methode pour calcul salire net
+	public double netSalary() {
+
+		return this.salary - this.salary * (this.socialRate / 100.0);
+	}
+
+	// methode pour trouver le nombre de jour avant le prochain anniversaire
+	public long daysBeforeBirthday() {
+		LocalDate currentDate = LocalDate.now();
+		LocalDate nextBirthday = LocalDate.parse(this.birthDay).withYear(currentDate.getYear());
+
+		if (nextBirthday.isBefore(currentDate) || nextBirthday.isEqual(currentDate)) {
+			nextBirthday = nextBirthday.plusYears(1);
+		}
+		return ChronoUnit.DAYS.between(currentDate, nextBirthday);
+	}
+
+	/**
+	 * Vérifie une chaîne de caractères et indique s'il s'agit d'un matricuel
+	 * correctement formaté ou non
+	 * 
+	 * @param inputToCheck La chaîne de caractère à vérifier
+	 * @return VRAI s'il s'agit d'un matricule, FAUX sinon
+	 */
+	private boolean checkRegistrationNumber(String inputToCheck) {
+
+		// Vérification de la taille de la chaîne de caractères
+		if (inputToCheck.length() != 7) {
+			return false;
+		}
+
+		// déclaration du booléen qui va stocker le résultat de la vérification
+		// VRAI -> la chaîne de caractères passée en paramètre est un matricule
+		// correctement formaté
+		// FAUX -> la chaîne de caractères passée en paramètre est un matricule pas bien
+		// formaté
+		boolean isRegistrationNumber = false;
+
+		// Cette première boucle permet de passer en revue TOUS les caractères de la
+		// chaîne
+		for (int index = 0; index < inputToCheck.length(); index++) {
+			// index = longueur de intupToCheck - 1 = 6
+			char ch = inputToCheck.charAt(index);
+			if (index == 0 || index == 1 || index == 5 || index == 6) {
+
+				// vérification du caractère, s'agit-il d'un chiffre ?
+				if (Character.isDigit(ch) == true) {
+					isRegistrationNumber = true;
+				} else { // attention, ce n'est pas un chiffre
+					return false;
+				}
+
+			} else { // cas de la position 2, 3 ou 4 -> vérification de letter
+
+				// Quelle opération dois-je faire ?
+				if (Character.isLetter(ch)) {
+					isRegistrationNumber = true;
+				} else {
+					return false;
+				}
+			}
+		} // fin du FOR, BRAVO !
+
+		return isRegistrationNumber;
+	}
+
+	private boolean checklastName(String inputToCheck) {
+		if (inputToCheck.length() > 15 || (inputToCheck.length() < 2)) {
+			return false;
+
+		}
+		boolean isLastName = false;
+		for (int i = 0; i < inputToCheck.length(); i++) {
+			char ch = inputToCheck.charAt(i);
+			if (Character.isLetter(ch) || ch == '-') {
+
+				isLastName = true;
+			} else {
+				return false;
+			}
+		}
+
+		return isLastName;
+	}
+
+	private boolean checkFirstName(String inputToCheck) {
+		if (inputToCheck.length() > 10 || (inputToCheck.length() < 2)) {
+			return false;
+
+		}
+		boolean isFirstName = false;
+		for (int i = 0; i < inputToCheck.length(); i++) {
+			char ch = inputToCheck.charAt(i);
+			if (Character.isLetter(ch)) {
+
+				isFirstName = true;
+			} else {
+				return false;
+			}
+		}
+
+		return isFirstName;
+	}
+
+	private boolean checkBirthDay(String dateToCheck) {
+		// Check if the length of the date string is exactly 10
+		if (dateToCheck.length() != 10) {
+			return false;
+		}
+
+		// Check each character of the date string
+		for (int index = 0; index < dateToCheck.length(); index++) {
+			char ch = dateToCheck.charAt(index);
+
+			// Positions 0-3, 5-6, 8-9 should be digits
+			if ((index == 0 || index == 1 || index == 2 || index == 3 ||
+					index == 5 || index == 6 || index == 8 || index == 9) && !Character.isDigit(ch)) {
+				return false;
+			}
+
+			// Positions 4 and 7 should be '-'
+			if ((index == 4 || index == 7) && ch != '-') {
+				return false;
+			}
+		}
+		return true;
+	}
+}
---
+++ src/main/java/fr/afpa/employees/EmployeeMain.java
@@ -0,0 +1,101 @@
+package fr.afpa.employees;
+
+import java.util.ArrayList;
+// import org.apache.logging.log4j.LogManager;
+// import org.apache.logging.log4j.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+// import java.time.LocalDate;
+// import java.time.temporal.ChronoUnit;
+
+/**
+ * Classe principale du projet, contient la fonction "main"
+ */
+class EmployeeMain {
+	private static final Logger logger = LoggerFactory.getLogger(EmployeeMain.class);
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("\n----- Exercice de programmation objet - classe \"Employee\" -----");
+		// checkRegistrationNumber();
+
+		// instanciez 4 objets de la classe "Employee" (une fois celle-ci implémentée)
+		Employee employee1 = new Employee("11ABC22, ", "Sari", "Farah", 1500.50, "1985-02-25");
+		Employee employee2 = new Employee("12ABC23, ", "Atmane", "Sara", 1300.20, "1996-02-05");
+		Employee employee3 = new Employee("13ABC24, ", "Laurent", "Patrick", 1800.20, "1996-02-05");
+		Employee employee4 = new Employee("14ABC25, ", "Prevoteau", "Jean-Pierre", 2000.0, "1996-02-05");
+
+		// System.out.println(employee1);
+
+		// afficher les informations des employés avec System.out.println
+		// System.out.println(employee1.toString());
+		// System.out.println(employee2.toString());
+		// System.out.println(employee3.toString());
+		// System.out.println(employee4.toString());
+		
+			 
+		
+ 
+
+System.out.println("Liste des employées: "); 
+// employee.forEach((n) -> print(n)); 
+for (Employee employee : employee) {
+	System.out.println(employee);
+}
+		employee.add(employee1);
+ 		employee.add(employee2);
+ 		employee.add(employee3);
+		employee.add(employee4);
+ System.out.println("Les employee sont " + (employee)); 
+
+
+
+		// pour gerer une exeption --> TRY..CATCH
+		try {
+			// Tentative de modification avec un matricule correctement formaté
+			employee1.setRegistrationNumber("54AZE88");
+
+			// test des matricules en erreur
+
+			// employee1.setRegistrationNumber("EEAZE88");
+			// employee1.setRegistrationNumber("6444433");
+			// employee1.setRegistrationNumber("64AZ");
+			// employee1.setRegistrationNumber("6");
+			// employee1.setRegistrationNumber("");
+			// employee1.setRegistrationNumber("987654");
+
+		} catch (Exception e) {
+			// CODE UTILISER POUR GERER L'EXEPTION
+			
+			logger.info("Le matricule n'est pas bon");
+		}
+		try {
+			employee1.setLastName("Azary");
+
+		} catch (Exception e) {
+			logger.info("Le nom n'est pas correcte ");
+
+		}
+		try {
+			employee1.setFirstName("Cloé");
+
+		} catch (Exception e) {
+			logger.info("Le prénom n'est pas bon");
+
+		}
+		try {
+			employee1.setBirthDay("1999-06-28");
+
+		} catch (Exception e) {
+			logger.info("La date de naissance n'est pas bonne");
+
+		}
+	}
+
+	public static ArrayList<Employee> employee = new ArrayList<Employee>();
+
+}
